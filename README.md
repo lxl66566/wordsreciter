3114b49185a8cc8df92a6c66e6d54bf9e547ba28
lxl66566 <1421962366@qq.com>
2022-05-25 10:34:27 +0800

    20220525-10:34

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # wordsreciter
+## 介绍
 这是一个轻量化的、简陋的英语背词python程序
 
 支持自己添加词库(.json)文件，自带10个词库
@@ -7,6 +8,14 @@
 
 ![image](https://user-images.githubusercontent.com/88281489/160550019-4aa8b438-81a0-4a6c-9b7b-a426e3112adc.png)
 ![image](https://user-images.githubusercontent.com/88281489/160550106-234ec3d7-df53-4b71-acfc-8b05ef729cf0.png)  
-<h4>
-  1.1版本(20220408)：新增日语词库与日语背词功能
-</h4>
+## 更新
+### v1.1(20220408)
+新增日语词库与日语背词功能
+### v2.0(20220525)
+为了更好地融入语言环境，v2.0版本将背词方式转为英-英与日-日，使用默认浏览器打开网页背词。（英：[牛津词典](https://www.oxfordlearnersdictionaries.com/) 日：[weblio词典](https://www.weblio.jp/)）
+
+同时保留v1.1的全部功能，若想使用原先方法背词请[更改](#settingsjson)`settings.json`。
+## 说明
+### settings.json
+`mode`：模式，1为网页打开模式，2为旧版背词模式
+`page_nums`：一次性打开网页的数量
--- reciter-2.0.py
+++ reciter-2.0.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 import json
 import os
-from random import choices
+from random import choices,randint
 from pathlib import Path
 import webbrowser
 
@@ -9,38 +9,63 @@ import webbrowser
 try:
     with open(os.path.dirname(__file__) + os.sep + 'settings.json','r',encoding='utf-8') as f:
         settingdic = json.load(f)
-    page_nums = settingdic["page_nums"]
 except FileNotFoundError:
-    settingdic = {"page_nums" : 8}
+    settingdic = {"mode" : 1,"page_nums" : 8}
     with open(os.path.dirname(__file__) + os.sep + 'settings.json','w',encoding='utf-8') as f:
         json.dump(settingdic,f,indent=4)
-    page_nums = 8
 
+page_nums = settingdic["page_nums"]
+mode = settingdic["mode"]
 filenme = os.listdir(os.path.dirname(__file__) + os.sep + 'words')
 temp1 = []
 for i in filenme:
     temp1.append(i.rstrip('.json'))
 for i in range(len(filenme)):
     print(i,temp1[i],sep=' ')
+if mode != 1:
+    for i in range(len(filenme)):
+        print(i + len(filenme),temp1[i] + ' by recite mode',sep=' ')
 cin = 0
 while(1):
     try:
         cin = int(input('Choose a dictionary to recite:'))
-        if 0 <= cin < len(filenme):
+        if 0 <= cin < len(filenme) * mode:
             break
         else:
             print('please input correct number!')
     except ValueError:
         print('please input a number!',end=' ')
+
+flag = False
+if cin >= len(filenme):
+    cin -= len(filenme)
+    flag = True
+
 with open(Path(os.path.dirname(__file__) + '\\words\\' + filenme[cin])) as f:
     data = json.load(f)
     k = list(data.keys())
     while(1):
-        s = choices(k,k= page_nums)
-        for i in s:
-            if filenme[cin] == '日本語.json':
-                url = f'https://www.weblio.jp/content/{i}'
-            else:
-                url = f'https://www.oxfordlearnersdictionaries.com/definition/english/{i}'
-            webbrowser.open(url,0,True)
+        if mode == 1:
+            s = choices(k,k= page_nums)
+            for i in s:
+                if filenme[cin] == '日本語.json':
+                    url = f'https://www.weblio.jp/content/{i}'
+                else:
+                    url = f'https://www.oxfordlearnersdictionaries.com/definition/english/{i}'
+                webbrowser.open(url,0,True)
+        elif mode == 2:
+            try:
+                s = randint(0,len(data) - 1)
+                if filenme[cin] == '日本語.json':
+                    print(k[s],end='')
+                else:
+                    print(k[s],end='   ')
+                if flag:
+                    t2 = input()
+                if filenme[cin] == '日本語.json':
+                    print(data[k[s]])
+                else:
+                    print(data[k[s]]['中释'].lstrip(' '),data[k[s]]['英释'],sep='  ')
+            except KeyError:
+                print(list(data[k[s]].values())[0])
         t = input()
