547bbec066a9f9d3bf4eeebacc6a6e8e03307f75
lxl66566 <18259734087@163.com>
2022-06-03 21:01:08 +0800

    v2.4

--- reciter.py
+++ reciter.py
@@ -9,16 +9,18 @@ settingdic = {
     "website" : 0,
     "auto_save_time" : 30
     }
-wrongdict = set([])
+LANGUAGE_MAP = ['english','japanese']
 DIRNAME = os.path.dirname(__file__)
 filename = os.listdir(DIRNAME + os.sep + 'words')
 itemname = []
 cin = -1
 WRONGNAME = 'notebook'
-ADD_WRONG_ITEM = '向 ' + WRONGNAME + ' 加入单词'
+ADD_WRONG_ITEM = ['向 ' + WRONGNAME + ' 加入英语单词','向 ' + WRONGNAME + ' 加入日语单词']
+wrongdict = {}
+language_set = [set(),set()]
 
 def loadjson():
-    global settingdic,wrongdict
+    global settingdic,wrongdict,language_set
     try:
         with open(DIRNAME + os.sep + 'settings.json','r',encoding='utf-8') as f:
             settingdic = json.load(f)
@@ -27,24 +29,27 @@ def loadjson():
             json.dump(settingdic,f,indent=4)
     try:
         with open(DIRNAME + os.sep + 'words' + os.sep + WRONGNAME + '.json','r',encoding='utf-8') as f:
-            temp = json.load(f)
-            wrongdict = set(temp['default'])
+            wrongdict = json.load(f)
+            language_set = [set(wrongdict[LANGUAGE_MAP[i]]['default']) for i in range(2)]
     except FileNotFoundError:
-        pass
+        for i in LANGUAGE_MAP:
+            wrongdict[i] = {'default' : []}
 
 # @atexit.register
 def commitwrong(s:str = ''):
-    if itemname[cin] != ADD_WRONG_ITEM:
+    if itemname[cin] not in ADD_WRONG_ITEM:
         return
     with open(DIRNAME + os.sep + 'words' + os.sep + WRONGNAME + '.json','w',encoding='utf-8') as f:
-        temp = {'default':list(wrongdict)}
-        json.dump(temp,f,indent=4)
+        for i in range(2):
+            wrongdict[LANGUAGE_MAP[i]]['default'] = list(language_set[i])
+        json.dump(wrongdict,f,indent=4)
     if s:
         print(s)
 
-def addwrongdict():
+def addwrongdict(language : int):# 0,1
+    language_str = LANGUAGE_MAP[language]
     print(
-'''这里是添加单词模式。请选择指令后回车。添加完毕后，请输入 exit 或 e 或 Ctrl+c 退出该模式。若直接关闭程序，可能不会添加单词。
+f'''这里是添加 {language_str} 单词模式。请选择指令后回车。添加完毕后，请输入 exit 或 e 或 Ctrl+c 退出该模式。若直接关闭程序，可能不会添加单词。
 
 添加单词：直接输入要添加的单词
 撤销上一个添加的单词：输入 undo 或 u
@@ -66,18 +71,18 @@ def addwrongdict():
                 if last == '':
                     print('撤销失败：未找到上一个单词')
                     continue
-                wrongdict.discard(last)
+                language_set[language].discard(last)
                 print('撤销成功！')
                 last = ''
             elif command.find('del') != -1:
                 try:
-                    wrongdict.remove(command.split()[-1].strip())
+                    language_set[language].remove(command.split()[-1].strip())
                     print('删除成功！')
                 except KeyError:
                     print('删除失败：未找到该单词')
             else:
                 last = command
-                wrongdict.add(last)
+                language_set[language].add(last)
                 print('添加成功！')
     except KeyboardInterrupt:
         pass
@@ -90,6 +95,19 @@ def print_item_names():
     for i in range(len(itemname)):
         print(i,itemname[i],sep=' ')
 
+def choose_language() -> int:
+    print('please choose a language:')
+    for i in range(len(LANGUAGE_MAP)):
+        print(i,LANGUAGE_MAP[i],sep=' ')
+    while True:
+        try:
+            temp = (int(input())) % len(LANGUAGE_MAP)
+            break
+        except ValueError:
+            print('please write a correct number! choose a language:')
+
+    return temp
+
 def reciter():
     global filename
     filename = os.listdir(DIRNAME + os.sep + 'words')
@@ -101,7 +119,8 @@ def reciter():
             itemname.remove(WRONGNAME)
         for i in range(len(itemname)):
             itemname.append(itemname[i] + ' - by recite mode')
-    itemname.append(ADD_WRONG_ITEM)
+    for i in ADD_WRONG_ITEM:
+        itemname.append(i)
     print_item_names()
     cin = 0
     while(1):
@@ -114,8 +133,8 @@ def reciter():
         except ValueError:
             print('please input a number!',end=' ')
 
-    if cin == len(itemname) - 1:    #最后一个
-        addwrongdict()
+    if itemname[cin] in ADD_WRONG_ITEM:    # 添加notebook
+        addwrongdict((cin - len(itemname) + 2) % len(ADD_WRONG_ITEM))# 0,1
         return
     flag = False
     if cin >= len(filename):
@@ -125,15 +144,17 @@ def reciter():
 
     with open(Path(DIRNAME + os.sep + 'words' + os.sep + filename[cin])) as f:
         data = json.load(f)
+        temp = 0
         if filename[cin] == WRONGNAME + '.json':
-            k = data['default']
+            temp = choose_language()
+            k = list(language_set[temp])
         else:
             k = list(data.keys())
         while(1):
             if mode == 1:
                 s = choices(k,k= page_nums)
                 for i in s:
-                    if filename[cin] == '日本語.json':
+                    if filename[cin] == '日本語.json' or temp:
                         url = f'https://www.weblio.jp/content/{i}'
                     else:
                         url = sitelib[website % len(sitelib)] + f'{i}'
